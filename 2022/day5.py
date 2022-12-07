e42cd609bc9ddae0247e5de633d6ee85b2d7de60
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-12-06 18:38:21 -0600

    Strip trailing whitespace removed important spaces

--- 2022/day5.py
+++ 2022/day5.py
@@ -63,10 +63,10 @@ def group_every(line: str, every: int, sep: int) -> list[str]:
 
 def run() -> None:
     "Synchronous entry point"
-    test_data = """    [D]
-[N] [C]
+    test_data = """    [D]    
+[N] [C]    
 [Z] [M] [P]
- 1   2   3
+ 1   2   3 
 
 move 1 from 2 to 1
 move 3 from 1 to 3
